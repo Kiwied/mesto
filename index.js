190c19acd06369bb462efaa4987e11bfad4cc977
Anton Anikeev <anik-anton@yandex.ru>
2020-04-13 21:50:22 +0300

    feat: js interaction

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let btnEdit = document.querySelector('.buttons__edit');
-let popup = document.querySelector('.popup');
-let btnClose = document.querySelector('.form__close');
+const editButton = document.querySelector('.buttons__edit');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.form__close');
+let profileName = document.querySelector('.profile__name');
+let formName = document.querySelector('.form__name');
+let profileDescription = document.querySelector('.profile__description');
+let formDescription = document.querySelector('.form__description');
+const saveButton = document.querySelector('.form__save');
 
 function open() {
+  formName.value = profileName.textContent;
+  formDescription.value = profileDescription.textContent;
   popup.classList.add('popup_opened');
 }
 
@@ -10,5 +17,13 @@ function close() {
   popup.classList.remove('popup_opened');
 }
 
-btnEdit.addEventListener('click', open);
-btnClose.addEventListener('click', close);
+function save() {
+  profileName.textContent = formName.value;
+  profileDescription.textContent = formDescription.value;
+  close();
+}
+
+editButton.addEventListener('click', open);
+closeButton.addEventListener('click', close);
+saveButton.addEventListener('click', save);
+
