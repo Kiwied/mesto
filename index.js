09ffcdeb67ca4d763f2bcb43c8c504870491c482
Anton Anikeev <anik-anton@yandex.ru>
2020-04-15 14:37:22 +0300

    refactor: <form> tag and BEM structure

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const editButton = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.form__close');
 let profileName = document.querySelector('.profile__name');
-let formName = document.querySelector('.form__name');
+let formName = document.querySelector('.form__input_name');
 let profileDescription = document.querySelector('.profile__description');
-let formDescription = document.querySelector('.form__description');
+let formDescription = document.querySelector('.form__input_description');
 const saveButton = document.querySelector('.form__save');
 
 function open() {
@@ -17,7 +17,8 @@ function close() {
   popup.classList.remove('popup_opened');
 }
 
-function save() {
+function save(e) {
+  e.preventDefault();
   profileName.textContent = formName.value;
   profileDescription.textContent = formDescription.value;
   close();
