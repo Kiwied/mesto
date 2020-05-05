099cb2dcbe096e485a552fc67dfef898a4864ba2
Anton Anikeev <anik-anton@yandex.ru>
2020-05-05 18:12:40 +0300

    feat/initial_cards

--- index.js
+++ index.js
@@ -6,6 +6,50 @@ let formName = document.querySelector('.form__input_name');
 let profileDescription = document.querySelector('.profile__description');
 let formDescription = document.querySelector('.form__input_description');
 const saveButton = document.querySelector('.form__save');
+const cardsContainer = document.querySelector('.elements');
+
+const initialCards = [
+  {
+      name: 'Карачаевск',
+      link: 'images/karachaevsk.jpg'
+  },
+  {
+      name: 'Гора Эльбрус',
+      link: 'images/elbrus.jpg'
+  },
+  {
+      name: 'Домбай',
+      link: 'images/dombai.jpg'
+  },
+  {
+      name: 'Сумба',
+      link: 'images/sumba.jpg'
+  },
+  {
+      name: 'Тироль',
+      link: 'images/tirol.jpg'
+  },
+  {
+      name: 'Сапай',
+      link: 'images/supai.jpg'
+  }
+];
+
+
+
+function addCard(nameValue, linkValue) {
+  const cardTemplate = document.querySelector('#card').content;
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.element__image').src = linkValue;
+  cardElement.querySelector('.element__title').textContent = nameValue;
+  cardElement.querySelector('.element__image').alt = nameValue;
+
+  cardsContainer.append(cardElement);
+};
+
+
+
 
 function open() {
   formName.value = profileName.textContent;
@@ -28,3 +72,4 @@ editButton.addEventListener('click', open);
 closeButton.addEventListener('click', close);
 saveButton.addEventListener('click', save);
 
+initialCards.forEach((element) => {addCard(element.name, element.link)});
