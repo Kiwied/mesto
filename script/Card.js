be3db2e2f1c60da092c381621490027cb549b706
Anton Anikeev <anik-anton@yandex.ru>
2020-06-03 23:41:22 +0300

    refactor: modules & new 'Card' class

---
+++ script/Card.js
@@ -0,0 +1,47 @@
+import {openPopup, closePopup, place, bigImage, bigCard, closeButtonBigCard} from "./index.js";
+
+class Card {
+  constructor(cardInfo, cardSelector) {
+    this._name = cardInfo.name;
+    this._link = cardInfo.link;
+    this._cardSelector = cardSelector;
+  }
+
+  _getTemplate() {
+    return document.querySelector(this._cardSelector).content.cloneNode(true);
+  }
+
+  generateCard() {
+    this._cardElement = this._getTemplate();
+    this._cardImageElement = this._cardElement.querySelector('.element__image');
+
+    this._cardImageElement.src = this._link;
+    this._cardElement.querySelector('.element__title').textContent = this._name;
+    this._cardImageElement.alt = this._name;
+
+    this._setEventListeners();
+
+    return this._cardElement;
+  }
+
+  _setEventListeners() {
+    closeButtonBigCard.addEventListener('click', () => closePopup(bigCard));
+
+    this._cardElement.querySelector('.element__like').addEventListener('click', (evt) => {
+      evt.target.classList.toggle('element__like_active');
+    });
+
+    this._cardElement.querySelector('.element__delete').addEventListener('click', (evt) => {
+      evt.target.closest('.element').remove();
+    }, {once: true});
+
+    this._cardImageElement.addEventListener('click', (evt) => {
+      place.textContent = evt.target.alt;
+      bigImage.alt = evt.target.alt;
+      bigImage.src = evt.target.src;
+      openPopup(bigCard);
+    });
+  }
+}
+
+export {Card};
--- script/index.js
+++ script/index.js
@@ -1,3 +1,5 @@
+import {Card} from './Card.js';
+
 const profile = document.querySelector('#popup__profile');
 const profileName = document.querySelector('.profile__name');
 const editButton = document.querySelector('.profile__edit');
@@ -19,7 +21,6 @@ const bigImage = bigCard.querySelector('.popup__image');
 const place = bigCard.querySelector('.popup__place');
 const closeButtonBigCard = bigCard.querySelector('.popup__close');
 
-const cardTemplate = document.querySelector('#card').content;
 
 const initialCards = [
   {
@@ -87,43 +88,6 @@ function resetValidation(popup) {
   });
 }
 
-function pressLike(evt) {
-  evt.target.classList.toggle('element__like_active');
-}
-
-function pressImage(evt) {
-  place.textContent = evt.target.alt;
-  bigImage.alt = evt.target.alt;
-  bigImage.src = evt.target.src;
-  openPopup(bigCard);
-}
-
-function pressDelete(evt) {
-  const card = evt.target.closest('.element');
-  card.querySelector('.element__like').removeEventListener('click', pressLike);
-  card.querySelector('.element__image').removeEventListener('click', pressImage);
-  card.remove();
-}
-
-function createCard(nameValue, linkValue) {
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardImageElement = cardElement.querySelector('.element__image');
-
-  cardImageElement.src = linkValue;
-  cardElement.querySelector('.element__title').textContent = nameValue;
-  cardImageElement.alt = nameValue;
-
-  cardElement.querySelector('.element__like').addEventListener('click', pressLike);
-  cardElement.querySelector('.element__delete').addEventListener('click', pressDelete, {once: true});
-  cardImageElement.addEventListener('click', pressImage);
-
-  return cardElement;
-};
-
-function addCard(nameValue, linkValue) {
-  cardsContainer.prepend(createCard(nameValue, linkValue));
-};
-
 editButton.addEventListener('click', () => {
   formName.value = profileName.textContent;
   formDescription.value = profileDescription.textContent;
@@ -139,7 +103,7 @@ addButton.addEventListener('click', () => {
 
 closeButtonProfile.addEventListener('click', () => closePopup(profile));
 closeButtonNewCard.addEventListener('click', () => closePopup(newCard));
-closeButtonBigCard.addEventListener('click', () => closePopup(bigCard));
+
 
 profile.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -149,14 +113,26 @@ profile.addEventListener('submit', (e) => {
 });
 newCard.addEventListener('submit', (e) => {
   e.preventDefault();
-  const name = newCardName.value;
-  const link = newCardLink.value;
-  addCard(name, link);
+  const newCardInfo =
+      {
+        name: newCardName.value,
+        link: newCardLink.value
+      };
+  const card = new Card(newCardInfo, '#card');
+  const cardElement = card.generateCard();
+  cardsContainer.prepend(cardElement);
+
   closePopup(newCard);
 });
 
-initialCards.forEach((element) => {addCard(element.name, element.link)});
+initialCards.forEach((element) => {
+  const card = new Card(element, '#card');
+  const cardElement = card.generateCard();
 
+  cardsContainer.prepend(cardElement);
+});
+
+export {openPopup, closePopup, place, bigImage, bigCard, closeButtonBigCard};
 
 
 
